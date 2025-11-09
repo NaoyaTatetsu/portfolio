14843a9edfe0fc4623a5565303cd7d2de7347e6c
NaoyaTatetsu <naoya.tatetsu@4digit.jp>
2025-11-09 20:31:32 +0900

    remove : scroll lock

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,7 +6,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import ScrollLock from "@/components/ScrollLock";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import type { Metadata } from "next";
 
@@ -52,7 +51,6 @@ export default async function LocaleLayout({
       >
         <ThemeProvider>
           <NextIntlClientProvider messages={messages}>
-            <ScrollLock />
             <div className="min-h-screen flex flex-col">
               <Header />
               <main className="flex-1 flex items-center justify-center pb-20 overflow-y-auto">
